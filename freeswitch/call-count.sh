ca9714ebe5e4368c597b43d23c96d4c7175186c3
dualtone-ben <ben.hybert@dualtone.co.uk>
2022-06-16 13:19:34 +0100

    Fixed new link for fscli perl script

--- freeswitch/call-count.sh
+++ freeswitch/call-count.sh
@@ -1,3 +1,3 @@
-if { /usr/local/ncpa/plugins/fscli-check.pl -q show-calls-count 2>&1 >&3 3>&- | grep '^' >&2; } 3>&1; then
+if { /usr/local/dualtone/sensu-checks/freeswitch/fscli-check.pl -q show-calls-count 2>&1 >&3 3>&- | grep '^' >&2; } 3>&1; then
   exit 1;
 fi
