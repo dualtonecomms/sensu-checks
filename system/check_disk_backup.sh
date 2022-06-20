ae81f83c85e8128f9a1c8d3d9e1b92c6ab061bba
dualtone-ben <ben.hybert@dualtone.co.uk>
2022-06-20 14:41:03 +0100

    Added 2 states for and extra echo output

--- system/check_disk_backup.sh
+++ system/check_disk_backup.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-THRESHOLD=90
+WARN=80
+CRIT=90
 df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read output;
 do
   used=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
   partition=$(echo $output | awk '{ print $2 }' )
-  if [ $used -ge $THRESHOLD ]; then
-    echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)"
-    exit 1;
-  else
+  if [ $used -lt $WARN ]; then
+    echo "Disk Usage is OK \"$partition ($used%)\" on $(hostname) as on $(date)"
     exit 0;
+  elif [ $used -lt $CRIT ]; then
+    echo "Disk Usage is WARNING \"$partition ($used%)\" on $(hostname) as on $(date)"
+    exit 1; 
+  else
+    echo "Disk Usage is CRITICAL \"$partition ($used%)\" on $(hostname) as on $(date)"
+    exit 2;
   fi
 done
