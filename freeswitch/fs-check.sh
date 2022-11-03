33c2583bc555ae404f4a66d45b5a7719801e9261
dualtone-ben <ben.hybert@dualtone.co.uk>
2022-11-03 15:51:38 +0000

    added new fs-check

---
+++ freeswitch/fs-check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CHECK=`fs_cli -x "sofia status profile internal"`
+
+if
+	echo "$CHECK" | grep -q "Invalid Profile!"; then
+	echo "$CHECK"
+	exit 2
+elif
+	echo "$CHECK" | grep -q "REGISTRATIONS"; then
+	echo "$CHECK"
+	exit 0
+else
+	echo "$CHECK"
+	exit 1
+fi
