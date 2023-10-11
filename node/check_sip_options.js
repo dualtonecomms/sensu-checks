ab3c0574e601e95feff6362f73781b8e005e70d8
dualtone-tony <39960654+dualtone-tony@users.noreply.github.com>
2023-10-11 15:38:23 +0100

    Update check_sip_options.js to include random sleep before finding free port
    Trying to avoid a clash of ports when scripts launch at the same time

--- node/check_sip_options.js
+++ node/check_sip_options.js
@@ -1,39 +1,42 @@
 const sip = require('sip');
 const fp = require("find-free-port")
 
-fp(3000, 3100, (err, freePort) => {
-    sip.start({
-        port: freePort
-    }, function (rq) {});
+const randomSleepTime = Math.floor(Math.random() * 1000)
 
-    function rstring() { return Math.floor(Math.random()*1e6).toString(); }
+setTimeout(() => {
+    fp(3000, 3100, (err, freePort) => {
+        sip.start({
+            port: freePort
+        }, function (rq) {});
     
-    const r = {
-        method: 'OPTIONS',
-        uri: process.argv[2],
-        headers: {
-            to: { uri: 'sip:ping@dualtone' },
-            from: { uri: 'sip:ping@dualtone', params: {tag: rstring()} },
-            'call-id': rstring(),
-            'User-Agent': 'Dualtone SIP Pinger',
-            cseq: { method: 'OPTIONS', seq: Math.floor(Math.random() * 1e5) },
-            'content-type': 'application/sdp',
-        }
-    };
-    
-    sip.send(r, (response) => {
-        if (response.status >= 200 && response.status < 300) {
-            console.log(`SIP OPTIONS packed to ${process.argv[2]} - Success: SIP response received with status: ${response.status} - ${response.reason}`);
-            process.exit(0);
-        }
-        else if (response.status >= 300 && response.status < 400) {
-            console.log(`SIP OPTIONS packed to ${process.argv[2]} - Warning: SIP response received with status: ${response.status} - ${response.reason}`);
-            process.exit(1);
-        }
-        else {
-            console.log(`SIP OPTIONS packed to ${process.argv[2]} - Failure: SIP response received with status: ${response.status} - ${response.reason}`);
-            process.exit(2);
-        }
-    });
-    
-})
+        function rstring() { return Math.floor(Math.random()*1e6).toString(); }
+        
+        const r = {
+            method: 'OPTIONS',
+            uri: process.argv[2],
+            headers: {
+                to: { uri: 'sip:ping@dualtone' },
+                from: { uri: 'sip:ping@dualtone', params: {tag: rstring()} },
+                'call-id': rstring(),
+                'User-Agent': 'Dualtone SIP Pinger',
+                cseq: { method: 'OPTIONS', seq: Math.floor(Math.random() * 1e5) },
+                'content-type': 'application/sdp',
+            }
+        };
+        
+        sip.send(r, (response) => {
+            if (response.status >= 200 && response.status < 300) {
+                console.log(`SIP OPTIONS packed to ${process.argv[2]} - Success: SIP response received with status: ${response.status} - ${response.reason}`);
+                process.exit(0);
+            }
+            else if (response.status >= 300 && response.status < 400) {
+                console.log(`SIP OPTIONS packed to ${process.argv[2]} - Warning: SIP response received with status: ${response.status} - ${response.reason}`);
+                process.exit(1);
+            }
+            else {
+                console.log(`SIP OPTIONS packed to ${process.argv[2]} - Failure: SIP response received with status: ${response.status} - ${response.reason}`);
+                process.exit(2);
+            }
+        });  
+    })
+}, randomSleepTime)
