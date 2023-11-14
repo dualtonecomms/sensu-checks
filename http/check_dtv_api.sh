2df2abf8548d7826ac8f2cdb6f7dc9f78ffa5802
dualtone-ben <ben.hybert@dualtone.co.uk>
2023-11-14 10:35:44 +0000

    add output

--- http/check_dtv_api.sh
+++ http/check_dtv_api.sh
@@ -11,6 +11,7 @@ expected_response='{"authenticated":false}'
 
 # Check if the response matches the expected response
 if echo "$response" | grep -q "^$expected_response$"; then
+    echo "The DTV API is running."
         exit 0
 else
     echo "The DTV API is not returning the expected output."
