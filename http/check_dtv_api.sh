dfaf81bc4bb7d11da96411b54ba13331e2d23a0c
dualtone-ben <ben.hybert@dualtone.co.uk>
2023-11-14 10:06:15 +0000

    add dtv_api check

---
+++ http/check_dtv_api.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# The URL to be checked
+URL="https://localhost:3000/api/v1/test"
+
+# Use curl to fetch the data from the URL, -k allows connections to SSL sites without certs
+response=$(curl -k -s "$URL")
+
+# The expected response
+expected_response='{"authenticated":false}'
+
+# Check if the response matches the expected response
+if echo "$response" | grep -q "^$expected_response$"; then
+        exit 0
+else
+    echo "The DTV API is not returning the expected output."
+        exit 2
+fi
